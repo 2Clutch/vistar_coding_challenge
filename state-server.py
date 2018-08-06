8d4766c8936d5bb18323b7a845d73f9c8bd5d1a2
Maximillian Naza <maximillianaza@gmail.com>
2018-08-06 17:44:06 -0400

    Define search method

--- state-server.py
+++ state-server.py
@@ -1,8 +1,22 @@
 import json
+from shapely.geometry import Polygon as pol, Point as p
+
 
 HOST_NAME = '127.0.0.1'
 PORT_NUMBER = 8080
 
+LO = 0  # longitude
+LA = 1  # latitude
+
 with open("states.json") as f:
     data = json.load(f)
     print(data)
+
+
+def search(cord):
+    if cord:
+        point = p(cord[LO], cord[LA])
+        for state in data:
+            polygon = pol(state['border'])
+            if polygon.contains(point):
+                return state['state']
