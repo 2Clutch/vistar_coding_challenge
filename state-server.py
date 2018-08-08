ad45c18aa0a44e575418a89dc0101adf58dd5479
Maximillian Naza <maximillianaza@gmail.com>
2018-08-08 18:49:55 -0400

    Parse command line argument using argpase. Approach not working

--- state-server.py
+++ state-server.py
@@ -1,36 +1,23 @@
 import json
-import socketserver
-from urllib.parse import urlparse, parse_qs
+import argparse
+from flask import Flask
 from shapely.geometry import Polygon as pol, Point as p
-from http.server import SimpleHTTPRequestHandler
 
-HOST_NAME = '127.0.0.1'
-PORT_NUMBER = 8080
-
-LO = 0  # longitude
-LA = 1  # latitude
-
+app = Flask(__name__)
 
 with open("states.json") as f:
     data = json.load(f)
 
 
-class RequestHandler(SimpleHTTPRequestHandler):
-    def do_get(self):
-        input_query = urlparse(self.path).query
-        if input_query:
-            query_lo_la = parse_qs(input_query)
-            if 'latitude' in query_lo_la and 'longitude' in query_lo_la:
-                self.send_response(200)
-                points = [float(query_lo_la['longitude'][0]), float(query_lo_la['latitude'][0])]
-                state_output = search(points)
-                if state_output:
-                    return state_output['state']
-
-
+@app.route('/', methods=['POST'])
 def search(cord):
+    parser = argparse.ArgumentParser()
+    lon = parser.add_argument("longitude", type=int)
+    lat = parser.add_argument("latitude", type=int)
+    args = parser.parse_args()
+
     if cord:
-        point = p(cord[LO], cord[LA])
+        point = p(cord[args[lon]], cord[args[lat]])
         for state in data:
             polygon = pol(state['border'])
             if polygon.contains(point):
@@ -38,11 +25,4 @@ def search(cord):
 
 
 if __name__ == '__main__':
-    httpd = socketserver.TCPServer((HOST_NAME, PORT_NUMBER), SimpleHTTPRequestHandler)
-    try:
-        print("Serving at: " + HOST_NAME + ":" + str(PORT_NUMBER))
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print("Server Stopped.")
+    app.run()
